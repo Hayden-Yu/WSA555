76fba1077d94ece594d63ce0228cd6409e5b8af1
Hayden <hyu99@myseneca.ca>
2018-04-02 01:14:07 -0400

    fix: enable prepareStatements to return auto generated keys

--- src/com/wsa500/business/SalesBusinessImpl.java
+++ src/com/wsa500/business/SalesBusinessImpl.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.LinkedList;
 import java.util.List;
 import javax.naming.InitialContext;
@@ -229,7 +230,7 @@ public class SalesBusinessImpl implements SalesBusiness {
 	}
 	
 	private PreparedStatement prepareStatement(Connection connection, String sql, Object[] params) throws SQLException {
-		PreparedStatement statement = connection.prepareStatement(sql);
+		PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 		for (int i = 0; i < params.length; i++) {
 			if (params[i] instanceof String) {
 				statement.setString(i + 1, (String)params[i]);
